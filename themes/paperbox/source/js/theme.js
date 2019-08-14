b57491858feecc7850824f4c8c9d94756c4c466a
flyash <fenxuehaiyan@qq.com>
2019-08-14 16:47:41 +0800

    Add comment wrapper

--- themes/paperbox/source/js/theme.js
+++ themes/paperbox/source/js/theme.js
@@ -41,13 +41,10 @@ $(function() {
     }
     if (url) window.open(url);
   });
-});
 
-
-(function($){
   /*cubeRotate start*/
   var isIE = function(){
-	return ("ActiveXObject" in window);
+    return ("ActiveXObject" in window);
   };
   if( isIE() ) {
     $('.contenedor').hide();
@@ -70,83 +67,4 @@ $(function() {
     // console.log('x=' + offsetleft + ', y=' + offsettop);
   }
   /* cubeRotate end*/
-
-  // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-    $('.qrcode').hide();
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
-
-    var $this = $(this),
-      type = $this.attr('data-share'),
-      offset = $this.offset();
-
-    var url = $this.attr('data-url'),
-    encodedUrl = encodeURIComponent(url),
-    id = 'article-share-box-' + $this.attr('data-id');
-
-    if ($('.' + id).length){
-      var box = $('.' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
-      }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-            '<a href="https://www.linkedin.com/shareArticle?url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="Linkedin"></a>',
-            '<a href="http://service.weibo.com/share/share.php?url=' + encodedUrl + '" class="article-share-weibo" target="_blank" title="Weibo"></a>',
-            '<a href="http://share.renren.com/share/buttonshare.do?link=' + encodedUrl + '" class="article-share-renren" target="_blank" title="Renren"></a>',
-            '<a href="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=' + encodedUrl + '" class="article-share-qq" target="_blank" title="Qzone"></a>',
-            '<a class="article-share-weixin" target="_blank" title="Weixin"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
-
-      var box = $(html);
-
-      $('body').append(box);
-    }
-
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-
-    $('.article-share-weixin').click(function(){
-      var e_qrcode = $(this).parents('.article-share-box').next('.qrcode');
-      if (e_qrcode.length){
-        e_qrcode.show();
-      } else {
-        $(this).parents('.article-share-box').after('<div class="qrcode"></div>');
-        e_qrcode = $(this).parents('.article-share-box').next('.qrcode');
-        e_qrcode.qrcode({
-          "render": "div",
-          "size": 180,
-          "text": encodeURI(url)
-        });
-        e_qrcode.css({
-          top: offset.top + 20,
-          left: offset.left -150,
-        });
-      }
-    });
-
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
-})(jQuery);
+});
